121111e9c35b4c5ba1b24904e0d04c60984ccbba
jv <ljw@live.jp>
2018-12-10 14:39:24 +0800

    fix: 🔥 instead Number

--- src/percentage.js
+++ src/percentage.js
@@ -1,1 +1,1 @@
-module.exports = (value, fixed = 0) => isNaN(value) ? value : `${(Number(value) * 100).toFixed(fixed)}%`
+module.exports = (value, fixed = 0) => isNaN(value) ? value : `${(parseFloat(value) * 100).toFixed(fixed)}%`
