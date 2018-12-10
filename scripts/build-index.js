583e11d68750d98c693fa6889d9a2553003b5622
jv <ljw@live.jp>
2018-12-10 14:23:08 +0800

    fix: 🔥 filter

--- scripts/build-index.js
+++ scripts/build-index.js
@@ -3,11 +3,13 @@ const path = require('path')
 
 let output = 'module.exports = {\n'
 
-const plugins = fs.readdirSync(path.resolve('./src'))
+const plugins = fs.readdirSync(path.resolve('./src')).filter(
+  name =>
+    path.extname(name) === '.js' && ['install.js', 'index.js'].indexOf(name) === -1
+)
+
 plugins.forEach((name, i) => {
-  if (path.extname(name) === '.js' && ['install.js', 'index.js'].indexOf(name) === -1) {
-    output += `  ${name.replace('.js', '')}: require('./${name}')${i === plugins.length - 4 ? '' : ','}\n`
-  }
+  output += `  ${name.replace('.js', '')}: require('./${name}')${i === plugins.length - 1 ? '' : ','}\n`
 })
 
 output += `}\n`
